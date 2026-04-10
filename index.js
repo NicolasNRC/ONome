8adf2a0f5eb700ffdbd4e7cc76bd63db89a384c1
NicolasNRC <nicolasromaoc@gmail.com>
2026-04-09 21:36:10 -0300

    Commit1

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from "express"
+
+const app = express ();
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+app.get("/", (req,res)=>{
+    res.send("certo")
+})
+
+app.get("/nome/:nome", (req,res)=>{
+const {nome} = req.params
+    res.send(`Seja Bem Vindo ${nome}`)
+
+})
+
+app.listen(3004,()=>{
+    console.log("sistema online")
+})
